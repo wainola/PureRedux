9ad4be4a6e112a49fc29da3e1642ca784aa95bfa
Nicolás Riquelme <nrriquelme@gmail.com>
2018-03-12 00:43:18 -0300

    reducers with initialState

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import {createStore} from 'redux';
-const reducer = (state, action) => {
-    if(action.type === "INC"){
-        return state+action.payload;
+import {combineReducers, createStore} from 'redux';
+
+const initialState = {
+    user:{
+        name:"nicolas",
+        age:30,
+    },
+    tweets:[]
+};
+
+// const reducer = (state, action) => {
+//     if(action.type === "INC"){
+//         return state+action.payload;
+//     }
+//     if(action.type ==="DEC"){
+//         return state-action.payload;
+//     }
+//     return state;
+// }
+
+const userReducer = (state=initialState.user, action) => {
+    switch(action.type){
+        case "CHANGE_NAME":
+            // returning the state as it is plus the changes
+            state = {...state, name:action.payload};
+            break;
+        case "CHANGE_AGE":
+            state = {...state, age:action.payload};
+            break;
     }
-    if(action.type ==="DEC"){
-        return state-action.payload;
-    }
+    // we will always return the state
     return state;
 }
-const store = createStore(reducer,0);
+const tweetReducer = (state=initialState.tweets, action) => {
+    return state;
+}
+
+const reducers = combineReducers({
+    user:userReducer,
+    tweets:tweetReducer
+})
+
+const store = createStore(reducers);
+
+console.log(store.getState());
 store.subscribe(() => {
     console.log("store changed", store.getState());
 });
-store.dispatch({type:"INC", payload:1});
-store.dispatch({type:"INC", payload:3});
-store.dispatch({type:"DEC", payload:34543532});
+
+store.dispatch({type:"CHANGE_NAME", payload:"Nicolas Riquelme"});
+store.dispatch({type:"CHANGE_AGE", payload:28});
+store.dispatch({type:"CHANGE_NAME", payload:"Camilo Riquelme"});
